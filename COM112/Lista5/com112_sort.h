b9ff47d13d0ba15a709fc9ff18c006952efd1098
Carlos Souza <carlossouza_94@hotmail.com>
2020-05-19 11:26:06 -0300

    Adicionado

---
+++ COM112/Lista5/com112_sort.h
@@ -0,0 +1,5 @@
+//Funções dos métodos de ordenação
+void insertionSort(int *V, int *n_compara, int *n_movimento, int n);
+void bubbleSort(int *V, int *n_compara, int *n_movimento, int n);
+void selectionSort(int *V, int *n_compara, int *n_movimento, int n);
+void mergeSort(int *V, int *n_compara, int *n_movimento, int inicio, int fim);
