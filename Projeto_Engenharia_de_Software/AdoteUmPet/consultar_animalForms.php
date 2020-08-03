605b1ca9dc6440adb1da2acc4636f1d6fd1d2cd2
Carlos Souza <carlossouza_94@hotmail.com>
2020-08-02 21:36:42 -0300

    Pasta Removida

--- Projeto_Engenharia_de_Software/AdoteUmPet/consultar_animalForms.php
+++
@@ -1,8 +0,0 @@
-<h1>Pesquisar animal</h1>
-<form method="POST" action="consultar_animal.php">
-    Pesquisar id:<input type="text" name="pesquisar" placeholder="DIGITE O ID">
-    <input type="submit" value="ENVIAR">
-</form>
-<form action="tela_admin.php">
-    <input type="submit" value="Voltar">
-</form>
